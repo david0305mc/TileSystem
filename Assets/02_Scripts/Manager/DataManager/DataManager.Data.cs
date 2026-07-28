069106c68d114dc5897e377b65d62c54d1652b1b
david0305 <david0305mc@gmail.com>
2026-07-28 15:16:16 +0900

    테이블 업데이트

--- Assets/02_Scripts/Manager/DataManager/DataManager.Data.cs
+++ Assets/02_Scripts/Manager/DataManager/DataManager.Data.cs
@@ -8,37 +8,6 @@ using UnityEngine.Scripting;
 
 public partial class DataManager : Singleton<DataManager> {
 	[Preserve]
-	public partial class Artifact {
-		public int id;
-		public string artifactname;
-		public string artifactdesc;
-		public string artifactlevelupdesc;
-		public string iconname;
-		public string modelname;
-		public int maxlevel;
-		public int levelupcost;
-		public UNLOCKTYPE unlocktype;
-		public int unlocktarget;
-		public int unlockvalue;
-		public string unlockdesc;
-	}
-	public Artifact[] ArtifactArray { get; private set; }
-	public Dictionary<int, Artifact> ArtifactDic { get; private set; }
-	[Preserve]
-	public void BindArtifactData(Type type, string text) {
-		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 2 });
-		GetType().GetProperty(nameof(ArtifactArray))?.SetValue(this, deserializedData, null);
-		ArtifactDic = ArtifactArray?.ToDictionary(i => i.id) ?? new Dictionary<int, Artifact>();
-	}
-	[Preserve]
-	public Artifact GetArtifactData(int _id) {
-		if (ArtifactDic != null && ArtifactDic.TryGetValue(_id, out Artifact value)) {
-			return value;
-		}
-		Debug.LogError($"테이블에 ID가 없습니다: {_id}");
-		return null;
-	}
-	[Preserve]
 	public partial class Localization {
 		public string id;
 		public string ko;
@@ -56,6 +25,89 @@ public partial class DataManager : Singleton<DataManager> {
 	[Preserve]
 	public Localization GetLocalizationData(string _id) {
 		if (LocalizationDic != null && LocalizationDic.TryGetValue(_id, out Localization value)) {
+			return value;
+		}
+		Debug.LogError($"테이블에 ID가 없습니다: {_id}");
+		return null;
+	}
+	[Preserve]
+	public partial class Furniture {
+		public int id;
+		public string namekey;
+		public string furnituretype;
+		public string prefabkey;
+		public int sizex;
+		public int sizey;
+		public int price;
+		public int unlocklevel;
+		public int capacity;
+		public int blocksmovement;
+	}
+	public Furniture[] FurnitureArray { get; private set; }
+	public Dictionary<int, Furniture> FurnitureDic { get; private set; }
+	[Preserve]
+	public void BindFurnitureData(Type type, string text) {
+		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 2 });
+		GetType().GetProperty(nameof(FurnitureArray))?.SetValue(this, deserializedData, null);
+		FurnitureDic = FurnitureArray?.ToDictionary(i => i.id) ?? new Dictionary<int, Furniture>();
+	}
+	[Preserve]
+	public Furniture GetFurnitureData(int _id) {
+		if (FurnitureDic != null && FurnitureDic.TryGetValue(_id, out Furniture value)) {
+			return value;
+		}
+		Debug.LogError($"테이블에 ID가 없습니다: {_id}");
+		return null;
+	}
+	[Preserve]
+	public partial class Recipe {
+		public int id;
+		public string namekey;
+		public string iconkey;
+		public int cookingfurnitureid;
+		public int cookingtime;
+		public int sellprice;
+		public int exp;
+		public int unlocklevel;
+	}
+	public Recipe[] RecipeArray { get; private set; }
+	public Dictionary<int, Recipe> RecipeDic { get; private set; }
+	[Preserve]
+	public void BindRecipeData(Type type, string text) {
+		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 2 });
+		GetType().GetProperty(nameof(RecipeArray))?.SetValue(this, deserializedData, null);
+		RecipeDic = RecipeArray?.ToDictionary(i => i.id) ?? new Dictionary<int, Recipe>();
+	}
+	[Preserve]
+	public Recipe GetRecipeData(int _id) {
+		if (RecipeDic != null && RecipeDic.TryGetValue(_id, out Recipe value)) {
+			return value;
+		}
+		Debug.LogError($"테이블에 ID가 없습니다: {_id}");
+		return null;
+	}
+	[Preserve]
+	public partial class Customer {
+		public int id;
+		public string namekey;
+		public string prefabkey;
+		public string movespeed;
+		public int eattime;
+		public int patiencetime;
+		public int ordergroupid;
+		public int rewardmultiplier;
+	}
+	public Customer[] CustomerArray { get; private set; }
+	public Dictionary<int, Customer> CustomerDic { get; private set; }
+	[Preserve]
+	public void BindCustomerData(Type type, string text) {
+		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 2 });
+		GetType().GetProperty(nameof(CustomerArray))?.SetValue(this, deserializedData, null);
+		CustomerDic = CustomerArray?.ToDictionary(i => i.id) ?? new Dictionary<int, Customer>();
+	}
+	[Preserve]
+	public Customer GetCustomerData(int _id) {
+		if (CustomerDic != null && CustomerDic.TryGetValue(_id, out Customer value)) {
 			return value;
 		}
 		Debug.LogError($"테이블에 ID가 없습니다: {_id}");
