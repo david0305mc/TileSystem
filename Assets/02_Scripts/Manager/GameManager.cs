de69ae03f8b211ee2f68dc72d0adb8e7e35a6d8a
david0305 <david0305mc@gmail.com>
2026-07-28 15:58:19 +0900

    폴더 구조 변경

--- Assets/02_Scripts/Manager/GameManager.cs
+++ Assets/02_Scripts/Manager/GameManager.cs
@@ -16,13 +16,25 @@ public class GameManager : SingletonMono<GameManager>
 
         try
         {
+            await DataManager.Instance.LoadDataAsync();
             UserDataManager.Instance.Init();
             await UserDataManager.Instance.LoadLocalDataAsync();
             await SceneTransition.Instance.LoadSceneWithFadeAsync(GameDefine.MainSceneName);
+
+            LoadComplete();
         }
         finally
         {
             _isStartingGame = false;
         }
+    }
+
+    private void LoadComplete()
+    {
+        foreach (var item in DataManager.Instance.FurnitureDic)
+        {
+            Debug.Log($"item {item.Value.namekey}");
+        }
+        
     }
 }
--- Assets/02_Scripts/Runtime/Manager/DataManager/DataManager.Data.cs
+++ Assets/02_Scripts/Runtime/Manager/DataManager/DataManager.Data.cs
@@ -8,29 +8,6 @@ using UnityEngine.Scripting;
 
 public partial class DataManager : Singleton<DataManager> {
 	[Preserve]
-	public partial class Localization {
-		public string id;
-		public string ko;
-		public string en;
-		public string jp;
-	}
-	public Localization[] LocalizationArray { get; private set; }
-	public Dictionary<string, Localization> LocalizationDic { get; private set; }
-	[Preserve]
-	public void BindLocalizationData(Type type, string text) {
-		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 2 });
-		GetType().GetProperty(nameof(LocalizationArray))?.SetValue(this, deserializedData, null);
-		LocalizationDic = LocalizationArray?.ToDictionary(i => i.id) ?? new Dictionary<string, Localization>();
-	}
-	[Preserve]
-	public Localization GetLocalizationData(string _id) {
-		if (LocalizationDic != null && LocalizationDic.TryGetValue(_id, out Localization value)) {
-			return value;
-		}
-		Debug.LogError($"테이블에 ID가 없습니다: {_id}");
-		return null;
-	}
-	[Preserve]
 	public partial class Furniture {
 		public int id;
 		public string namekey;
@@ -47,7 +24,7 @@ public partial class DataManager : Singleton<DataManager> {
 	public Dictionary<int, Furniture> FurnitureDic { get; private set; }
 	[Preserve]
 	public void BindFurnitureData(Type type, string text) {
-		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 2 });
+		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 1 });
 		GetType().GetProperty(nameof(FurnitureArray))?.SetValue(this, deserializedData, null);
 		FurnitureDic = FurnitureArray?.ToDictionary(i => i.id) ?? new Dictionary<int, Furniture>();
 	}
@@ -74,7 +51,7 @@ public partial class DataManager : Singleton<DataManager> {
 	public Dictionary<int, Recipe> RecipeDic { get; private set; }
 	[Preserve]
 	public void BindRecipeData(Type type, string text) {
-		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 2 });
+		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 1 });
 		GetType().GetProperty(nameof(RecipeArray))?.SetValue(this, deserializedData, null);
 		RecipeDic = RecipeArray?.ToDictionary(i => i.id) ?? new Dictionary<int, Recipe>();
 	}
@@ -101,13 +78,37 @@ public partial class DataManager : Singleton<DataManager> {
 	public Dictionary<int, Customer> CustomerDic { get; private set; }
 	[Preserve]
 	public void BindCustomerData(Type type, string text) {
-		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 2 });
+		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 1 });
 		GetType().GetProperty(nameof(CustomerArray))?.SetValue(this, deserializedData, null);
 		CustomerDic = CustomerArray?.ToDictionary(i => i.id) ?? new Dictionary<int, Customer>();
 	}
 	[Preserve]
 	public Customer GetCustomerData(int _id) {
 		if (CustomerDic != null && CustomerDic.TryGetValue(_id, out Customer value)) {
+			return value;
+		}
+		Debug.LogError($"테이블에 ID가 없습니다: {_id}");
+		return null;
+	}
+	[Preserve]
+	public partial class Level {
+		public int id;
+		public int requiredexp;
+		public int rewardgold;
+		public int maxtablecount;
+		public int maxstaffcount;
+	}
+	public Level[] LevelArray { get; private set; }
+	public Dictionary<int, Level> LevelDic { get; private set; }
+	[Preserve]
+	public void BindLevelData(Type type, string text) {
+		var deserializedData = CSVSerializer.Deserialize(text, type, new CSVSerializer.Options() { SkipDataRows = 1 });
+		GetType().GetProperty(nameof(LevelArray))?.SetValue(this, deserializedData, null);
+		LevelDic = LevelArray?.ToDictionary(i => i.id) ?? new Dictionary<int, Level>();
+	}
+	[Preserve]
+	public Level GetLevelData(int _id) {
+		if (LevelDic != null && LevelDic.TryGetValue(_id, out Level value)) {
 			return value;
 		}
 		Debug.LogError($"테이블에 ID가 없습니다: {_id}");
