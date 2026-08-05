ba2332387aa63875172a75c66e6f1cfbcb9f5253
david0305 <david0305mc@gmail.com>
2026-08-05 15:26:13 +0900

    camera position clamp

--- Assets/02_Scripts/Runtime/Manager/CameraControllerTest.cs
+++ Assets/02_Scripts/Runtime/Manager/CameraControllerTest.cs
@@ -7,6 +7,8 @@ public class CameraControllerTest : MonoBehaviour
 
     [SerializeField] private Camera _camera;
 
+    [SerializeField] private Vector2 _worldMin;
+    [SerializeField] private Vector2 _worldMax;
 
     private bool _isDragging;
     private Vector2 _previousPosition;
@@ -61,8 +63,9 @@ public class CameraControllerTest : MonoBehaviour
         Vector3 prevWorldPos = ScreenToWorldPosition(_camera, _previousPosition);
         Vector3 currWorldPos = ScreenToWorldPosition(_camera, pointerPosition);
 
-        var diff = prevWorldPos - currWorldPos;
-        _camera.transform.position += new Vector3(diff.x, diff.y, 0) ;
+        var delta = prevWorldPos - currWorldPos;
+        _camera.transform.position += new Vector3(delta.x, delta.y, 0);
+        ClampCameraPosition();
 
         _previousPosition = pointerPosition;
     }
@@ -70,6 +73,16 @@ public class CameraControllerTest : MonoBehaviour
     private void EndDrag()
     {
         _isDragging = false;
+    }
+
+    private void ClampCameraPosition()
+    {
+        float halfHeight = _camera.orthographicSize;
+        float halfWidth = halfHeight * _camera.aspect;
+
+        var x = Mathf.Clamp(_camera.transform.position.x, _worldMin.x + halfWidth, _worldMax.x - halfWidth);
+        var y = Mathf.Clamp(_camera.transform.position.y, _worldMin.y + halfHeight, _worldMax.y - halfHeight);
+        _camera.transform.position = new Vector3(x, y, _camera.transform.position.z);
     }
 
     public static Vector3 ScreenToWorldPosition(Camera camera, Vector3 worldPos)
