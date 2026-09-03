ddd1bda424041b5f25d510d071578bf19a15d1ae
david0305 <david0305mc@gmail.com>
2026-09-03 16:02:33 +0900

    테이블 업데이트

--- Assets/02_Scripts/Runtime/Manager/DataManager/ConfigTable.cs
+++ Assets/02_Scripts/Runtime/Manager/DataManager/ConfigTable.cs
@@ -7,34 +7,6 @@ using UnityEngine.Scripting;
 
 public class ConfigTable : Singleton<ConfigTable> {
 	[Preserve]
-	public int ShowDevilSayTextCoolTime;
-	[Preserve]
-	public int OfflineRewardMinTime;
-	[Preserve]
-	public int OfflineRewardMaxTime;
-	[Preserve]
-	public int OfflineRewardRate;
-	[Preserve]
-	public int FreeGachaCooltime;
-	[Preserve]
-	public int MaxFishCreate;
-	[Preserve]
-	public int FishTankSetDefaultCount;
-	[Preserve]
-	public int StoneTalkMax;
-	[Preserve]
-	public int  FishTalkMax;
-	[Preserve]
-	public int StoneGrowUp_1;
-	[Preserve]
-	public int StoneGrowUp_2;
-	[Preserve]
-	public int StoneGrowUp_3;
-	[Preserve]
-	public int StartHeart;
-	[Preserve]
-	public int StoneLevelMax;
-	[Preserve]
 	public int share_event_count;
 
 	[Preserve]
